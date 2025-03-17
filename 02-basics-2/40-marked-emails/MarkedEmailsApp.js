203a8c502f000c3f64574b5ba4f3e253989c2530
Julia Gordeeva <gordeeva.ia@parma.ru>
2025-03-17 23:12:30 +0500

    Solve Marked-emails

--- 02-basics-2/40-marked-emails/MarkedEmailsApp.js
+++ 02-basics-2/40-marked-emails/MarkedEmailsApp.js
@@ -32,19 +32,32 @@ export const emails = [
 export default defineComponent({
   name: 'MarkedEmailsApp',
 
-  setup() {},
+  setup() {
+    const searchValue = ref("")
+
+    const markedEmails = computed(() => {
+      return emails.map((email) => { 
+        return { 
+          value: email, 
+          isMarked: searchValue.value && email.toLowerCase().includes(searchValue.value.toLowerCase()) 
+        }
+      })
+    })
+
+    return {
+      markedEmails,
+      searchValue
+    }
+  },
 
   template: `
     <div>
       <div class="form-group">
-        <input type="search" aria-label="Search" />
+        <input type="search" aria-label="Search" v-model="searchValue"/>
       </div>
       <ul aria-label="Emails">
-        <li>
-          Eliseo@gardner.biz
-        </li>
-        <li class="marked">
-          Jayne_Kuhic@sydney.com
+        <li v-for="email in markedEmails" :class="{ marked: email.isMarked }">
+          {{ email.value }}
         </li>
       </ul>
     </div>
