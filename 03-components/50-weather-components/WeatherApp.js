ea50f8446dd7ea07b8fd96c080cecee3407df91a
Julia Gordeeva <gordeeva.ia@parma.ru>
2025-04-07 01:39:04 +0500

    fix WeatherComponents

--- 03-components/50-weather-components/WeatherApp.js
+++ 03-components/50-weather-components/WeatherApp.js
@@ -14,21 +14,16 @@ export default defineComponent({
     return {
       weatherData: getWeatherData(),
       weatherIcons: WeatherConditionIcons,
-      isNight: (weatherInfo) => {
-          const {dt: currentTime, sunrise, sunset} = weatherInfo
-          return currentTime < sunrise || currentTime > sunset
-      }
     }
   },
 
   template: `
     <div>
       <h1 class="title">Погода в Средиземье</h1>
-
       <ul class="weather-list unstyled-list">
-        <li v-for="weather in weatherData" class="weather-card" :class="{ 'weather-card--night': isNight(weather.current) } ">
-          <WeatherCard :weatherInfo="weather" :weatherIcon="weatherIcons[weather.current.weather.id]"/>
-        </li>
+        <WeatherCard v-for="weather in weatherData"
+                     :weatherInfo="weather"
+                     :weatherIcon="weatherIcons[weather.current.weather.id]"/>
       </ul>
     </div>
   `,
--- 03-components/50-weather-components/WeatherCard.js
+++ 03-components/50-weather-components/WeatherCard.js
@@ -1,8 +1,8 @@
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import WeatherCardAlert from './WeatherCardAlert'
 import WeatherCardTitle from './WeatherCardTitle'
-import WeatherCardConditions from "./WeatherCardConditions.js";
-import WeatherDetails from "./WeatherDetails.js";
+import WeatherCardConditions from './WeatherCardConditions.js'
+import WeatherDetails from './WeatherDetails.js'
 
 export default defineComponent({
   name: 'WeatherCard',
@@ -26,26 +26,34 @@ export default defineComponent({
   },
 
   setup(props) {
+    const isNight = computed(() => {
+      const { dt: currentTime, sunrise, sunset } = props.weatherInfo.current
+      return currentTime < sunrise || currentTime > sunset
+    })
+
     return {
       weather: props.weatherInfo,
       weatherIcon: props.weatherIcon,
+      isNight,
     }
   },
 
   template: `
-    <WeatherCardAlert v-if="weather.alert"
-                      :senderName="weather.alert.sender_name"
-                      :description="weather.alert.description"
-    />
-    <WeatherCardTitle :name="weather.geographic_name"
-                      :time="weather.current.dt"
-    />
-    <WeatherCardConditions :description="weather.current.weather.description"
-                           :icon="weatherIcon"
-                           :temperature="weather.current.temp"
-    />
-    <WeatherDetails :pressure="weather.current.pressure" :humidity="weather.current.humidity"
-                    :clouds="weather.current.clouds" :wind-speed="weather.current.wind_speed"
-    />
+    <li class="weather-card" :class="{ 'weather-card--night': isNight }">
+      <WeatherCardAlert v-if="weather.alert"
+                        :senderName="weather.alert.sender_name"
+                        :description="weather.alert.description"
+      />
+      <WeatherCardTitle :name="weather.geographic_name"
+                        :time="weather.current.dt"
+      />
+      <WeatherCardConditions :description="weather.current.weather.description"
+                             :icon="weatherIcon"
+                             :temperature="weather.current.temp"
+      />
+      <WeatherDetails :pressure="weather.current.pressure" :humidity="weather.current.humidity"
+                      :clouds="weather.current.clouds" :wind-speed="weather.current.wind_speed"
+      />
+    </li>
   `,
 })
